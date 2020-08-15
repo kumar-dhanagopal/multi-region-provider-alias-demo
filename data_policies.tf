3027da429497b126e361581a3456e917dffb77bd
Kumar Dhanagopal <kumar.dhanagopal@oracle.com>
2020-08-14 22:27:02 -0700

    Update data_policies.tf

--- data_policies.tf
+++ data_policies.tf
@@ -1,8 +1,8 @@
-data "oci_identity_policies" "policies" {
+data "oci_identity_policies" "policies_primary_region" {
 provider = oci.primary_region
     compartment_id = var.compartment
 }
-data "oci_identity_policies" "policies" {
+data "oci_identity_policies" "policies_standby_region" {
 provider = oci.standby_region
     compartment_id = var.compartment
 }
