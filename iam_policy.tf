89bc3edf465ba56f878b11443237bba7a57cf81f
kumar_dhanagopal <kumar.dhanagopal@oracle.com>
2020-08-15 12:35:03 -0700

    changes

--- iam_policy.tf
+++ iam_policy.tf
@@ -1,4 +1,5 @@
 resource "oci_identity_policy" "object_storage_replication" {
+  provider       = oci.home_region
   compartment_id = var.compartment
   description    = "Policy to permit the Object Storage service in the primary region to replicate objects to the standby region"
   name           = "iam_object_storage_policy"
--- provider.tf
+++ provider.tf
@@ -1,4 +1,12 @@
 provider "oci" {
+  alias            = "home_region"
+  tenancy_ocid     = var.tenancy
+  user_ocid        = var.user
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key
+  region           = data.oci_identity_tenancy.tenancy_details.home_region_key
+}
+provider "oci" {
   alias            = "primary_region"
   tenancy_ocid     = var.tenancy
   user_ocid        = var.user
